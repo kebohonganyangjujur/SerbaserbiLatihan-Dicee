7b2cbc7c021c21764ed38b770d63f1aa6438819b
kebohonganyangjujur <kebohonganyangjujur@gmail.com>
2022-06-14 07:16:03 +0700

    complete randomize dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dicee1: UIImageView!
+    @IBOutlet weak var dicee2: UIImageView!
+    var diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        dicee1.image = diceArray.randomElement()
+        dicee2.image = diceArray.randomElement()
+    }
+    
 }
 
